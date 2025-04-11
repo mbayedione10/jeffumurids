2a6f2937d5aa9c1f41b4946429cfbb8ad8452164
mbayedione10 <mbayedione10@gmail.com>
2025-04-11 13:16:04 +0000

    update

---
+++ src/data.ts
@@ -0,0 +1,10 @@
+export const testimonials = [
+  {
+    name: "Abdoul, disciple mouride",
+    quote: "Participer au financement des ndogous collectifs est une façon pour moi de me sentir connecté à ma communauté même depuis l'étranger. C'est une tradition qui nous définit et nous rassemble."
+  },
+  {
+    name: "Fatou, bénévole",
+    quote: "Les ndogous collectifs sont bien plus qu'un simple repas. C'est un moment d'unité, de partage et de spiritualité qui nous permet de vivre pleinement notre foi et nos traditions."
+  }
+];
